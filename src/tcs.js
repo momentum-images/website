d30bf3e416683c0809792a073df927365b8688d5
Luke Preston <lukejpreston@gmail.com>
2020-11-14 21:23:50 +0000

    stuff

--- src/tcs.js
+++ src/tcs.js
@@ -5,7 +5,7 @@ import { faFilePdf } from '@fortawesome/free-regular-svg-icons'
 export default () => (
   <div className='tcs is-lato'>
     <p className='content'>By using any of the images produced by Momentum Images or Melanie Preston you agree to the Terms and Conditions</p>
-    <a href='/tcs.pdf'>
+    <a href='https://momentumimages.co.uk/tcs.pdf'>
       <span className='icon'>
         <FontAwesomeIcon icon={faFilePdf} />
       </span>
