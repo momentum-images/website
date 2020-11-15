dcf24269f32197f6fa9ae644f1b5f80692a68a62
Luke Preston <lukejpreston@gmail.com>
2020-11-15 14:12:01 +0000

    service worker stuff

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './index.css'
 import React from 'react'
 import { hydrate, render } from 'react-dom'
 import App from './app'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
 const rootElement = document.getElementById('root')
 if (rootElement.hasChildNodes()) {
@@ -12,4 +13,9 @@ if (rootElement.hasChildNodes()) {
   render(<App />, rootElement)
 }
 
-serviceWorker.register()
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log)
