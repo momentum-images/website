efa800d30f368e53ed270d293862cf340f07f034
Luke Preston <lukejpreston@gmail.com>
2020-11-15 13:31:37 +0000

    hydrate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import 'bulma/css/bulma.css'
 import './index.css'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import App from './app'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
 
 serviceWorker.register()
