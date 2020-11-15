cfbdbabb5749ee002d12775700209aa22c1c3fe8
Luke Preston <lukejpreston@gmail.com>
2020-11-15 22:07:10 +0000

    removing report

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { hydrate, render } from 'react-dom'
 import App from './app'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 
 const rootElement = document.getElementById('root')
 if (rootElement.hasChildNodes()) {
@@ -18,4 +18,4 @@ serviceWorkerRegistration.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log)
+// reportWebVitals(console.log)
