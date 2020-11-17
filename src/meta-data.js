ecb906cd89b1a9b87203b9fe9d40848b4bf63aa1
Luke Preston <lukejpreston@gmail.com>
2020-11-17 07:36:03 +0000

    update meta data

--- src/meta-data.js
+++ src/meta-data.js
@@ -8,20 +8,20 @@ export default ({ title, description, path, image }) => (
     <meta name='og:title' content={title} />
     <meta name='description' content={description} />
     <meta name='og:description' content={description} />
-    <meta name='og:url' content={`https://momentumimages.co.uk${path}`} />
+    <meta name='og:url' content={`https://momentumimages.co.uk/${path}`} />
 
     <title>{title}</title>
     <meta name='title' content={title} />
     <meta name='description' content={description} />
 
     <meta property='og:type' content='website' />
-    <meta property='og:url' content={`https://momentumimages.co.uk${path}`} />
+    <meta property='og:url' content={`https://momentumimages.co.uk/${path}`} />
     <meta property='og:title' content={title} />
     <meta property='og:description' content={description} />
     <meta property='og:image' content={image} />
 
     <meta property='twitter:card' content='summary_large_image' />
-    <meta property='twitter:url' content={`https://momentumimages.co.uk${path}`} />
+    <meta property='twitter:url' content={`https://momentumimages.co.uk/${path}`} />
     <meta property='twitter:title' content={title} />
     <meta property='twitter:description' content={description} />
     <meta property='twitter:image' content={image} />
